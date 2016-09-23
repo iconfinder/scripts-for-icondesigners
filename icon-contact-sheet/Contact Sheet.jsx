7db660feb5895e6d424629a8a1a862653576fdc3
Scott Lewis <scott@iconify.it>
2016-09-23 19:27:08 +0200

    Cleaned up code. Added filename input field

--- icon-contact-sheet/Contact Sheet.jsx
+++ icon-contact-sheet/Contact Sheet.jsx
@@ -4,8 +4,8 @@ var originalInteractionLevel = userInteractionLevel;
 userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS;
 
 /**
-*  @author   Iconfinder.com - http://iconfinder.com
- * @date        2016-09-13
+*  @author  Iconfinder.com - http://iconfinder.com
+ * @date    2016-09-13
  *
  *  Installation: 
  *
@@ -41,16 +41,16 @@ userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS;
  */
  
 var CONFIG = {
-    ROWS: 60,
+    ROWS: 12,
     COLS: 10,
-    VOFF: 30,
-    HOFF: 30,
+    VOFF: 20,
+    HOFF: 20,
     FRM_WIDTH: 100,
     FRM_HEIGHT: 100,
     ROW_HEIGHT: 100,
     COL_WIDTH: 100,
-    PG_WIDTH: 1060,
-    PG_HEIGHT: 6000,
+    PG_WIDTH: 800,
+    PG_HEIGHT: 1200,
     PG_UNITS: "px",
     GUTTER: 0,
     SCALE: 100,
@@ -58,45 +58,48 @@ var CONFIG = {
     LOG_FILE_PATH: "~/Desktop/ai-contactsheet-log.txt",
     CHOOSE_FOLDER: "Please choose your Folder of files to place…",
     NO_SELECTION: "No selection",
-    COMPOSITE_FILE_NAME: "contact-sheet.ai",
+    LAYER_NOT_CREATED: "Could not create layer. ",
+    
+    AIFORMAT: Compatibility.ILLUSTRATOR10,
     
     SHRINK_TO_FIT: true,
     
     START_FOLDER: "~/Desktop",
     
+    FILENAME: "contact-sheet",
+    
     LOGGING: true,
     
     SKIP_COLS: 0, 
-    DEBUG: false
+    DEBUG: false,
+    STRIP: ["svg", "ai", "eps", "txt", "pdf"]
 }
 
+/**
+ * Displays the settings dialog
+ *
+ * Inputs:
+ *	- skip columns
+ *	- page width
+ *	- page height
+ *	- cell width
+ *	- cell height
+ *	- scale
+ *	- logging enabled
+ *	
+ *	- number of cols        = divide page width by cell width
+ *	- number of rows        = divide page height by cell height
+ *	- side margins          = (page width - (col count * col width))/2
+ *	- top/bottom margins    = (page height - (row count * row width))/2
+ *
+ * @return Settings object
+ */
 function doDisplayDialog() { 
 
 	var dialog = new Window("dialog", "Contact Sheet Settings", [550, 350, 900, 700]); 
 	var response = false;
 	
-	/*
-	Inputs:
-	- skip columns
-	- page width
-	- page height
-	- cell width
-	- cell height
-	- scale
-	- logging enabled
-	
-	- number of cols        = divide page width by cell width
-	- number of rows        = divide page height by cell height
-	- side margins          = (page width - (col count * col width))/2
-	- top/bottom margins    = (page height - (row count * row width))/2
-	*/
-	
-	dialog.skipColsLabel        = dialog.add("statictext", [32, 30, 132, 60], "Skip Columns");
-	dialog.skipCols             = dialog.add("edittext", [150, 30, 200, 60], CONFIG.SKIP_COLS); 
-	dialog.skipCols.active      = true;
-	
 	try {
-        // page width & height - [32, 30, 132, 60] [X1, Y1, X2, Y2] X2 - X1 = W, Y2 - Y1 = H
         dialog.pageWidthLabel       = dialog.add("statictext", [32, 30, 132, 60], "Page Width:");
         dialog.pageWidth            = dialog.add("edittext", [150, 30, 200, 60], CONFIG.PG_WIDTH); 
         dialog.pageWidth.active     = true;
@@ -116,8 +119,12 @@ function doDisplayDialog() {
         dialog.scaleLabel           = dialog.add("statictext", [32, 190, 132, 220], "Scale:");
         dialog.scale                = dialog.add("edittext", [150, 190, 200, 220], CONFIG.SCALE); 
         dialog.scale.active         = true;
+        
+        dialog.filenameLabel        = dialog.add("statictext", [32, 230, 132, 260], "File Name:");
+        dialog.filename             = dialog.add("edittext", [150, 230, 320, 260], CONFIG.FILENAME); 
+        dialog.filename.active      = true;
     
-        dialog.logging              = dialog.add('checkbox', [32, 270, 132, 300], "Logging?");
+        dialog.logging              = dialog.add('checkbox', [32, 270, 132, 340], "Logging?");
         dialog.logging.value        = CONFIG.LOGGING;
 
         dialog.cancelBtn            = dialog.add("button", [80,300,170,330], "Cancel", {name:"cancel"});
@@ -131,7 +138,6 @@ function doDisplayDialog() {
     
         dialog.openBtn.onClick = function() { 
 
-    // 		CONFIG.SKIP_COLS    = parseInt(dialog.skipCols.text); 
             CONFIG.PG_WIDTH     = parseInt(dialog.pageWidth.text); 
             CONFIG.PG_HEIGHT    = parseInt(dialog.pageHeight.text); 
             CONFIG.COL_WIDTH    = parseInt(dialog.colWidth.text);
@@ -172,6 +178,30 @@ function doDisplayDialog() {
     return response;
 }
 
+/**
+ * Utility function to strip the file extension from a user-supplied file name
+ * @param <string> filename
+ * @return <string> The new file name sans extension
+ */
+function stripFileExtension(filename) {
+    var bits = filename.split(".");
+    var bit  = bits[bits.length-1];
+    var found = false;
+    if (bits.length > 1 && bit) {
+        for (ext in CONFIG.STRIP) {
+            if (ext.toLowerCase() == bit.toLowerCase()) {
+                found = true;
+            }
+        }
+    }
+    if (found) bits = bits[bits.length-1] = "";
+    return bits.join(".");
+}
+
+/**
+ * Main logic to create the contact sheet.
+ * @return void
+ */
 function doCreateContactSheet() {  
   
     var doc, fileList, i, srcFolder, svgFile, 
@@ -181,8 +211,6 @@ function doCreateContactSheet() {
     var saveCompositeFile = false;
 
     srcFolder = Folder.selectDialog(CONFIG.CHOOSE_FOLDER, CONFIG.START_FOLDER);
-    
-    CONFIG.COMPOSITE_FILE_NAME = srcFolder.name.replace(" ", "-") + ".ai";
 
     if (srcFolder != null) {  
     
@@ -214,6 +242,11 @@ function doCreateContactSheet() {
             if (! doDisplayDialog()) {
                 return;
             }
+            
+            if (CONFIG.FILENAME.replace(" ", "") == "") {
+                CONFIG.FILENAME = srcFolder.name.replace(" ", "-") + "-all";
+            }
+            // CONFIG.FILENAME = stripFileExtension(CONFIG.FILENAME);
         
             app.coordinateSystem = CoordinateSystem.ARTBOARDCOORDINATESYSTEM;  
 
@@ -303,7 +336,7 @@ function doCreateContactSheet() {
                                         theLayer.name = f.name;
                                     }
                                     catch(ex) {
-                                        logger("Could not create layer. " + ex);
+                                        logger(CONFIG.LAYER_NOT_CREATED + ex);
                                     }
                                     svgFile = doc.groupItems.createFromFile(f); 
                             
@@ -351,7 +384,7 @@ function doCreateContactSheet() {
                     }
                 };
                 if (saveCompositeFile)
-                    saveFileAsAi(srcFolder.path + "/" + CONFIG.COMPOSITE_FILE_NAME);
+                    saveFileAsAi(srcFolder.path + "/" + CONFIG.FILENAME);
             }
         }; 
     };  
@@ -359,8 +392,8 @@ function doCreateContactSheet() {
 
 /**
  * Arranges items in the selection on a grid
- * param selection sel    The current selection
- * return void
+ * @param <selection> sel    The current selection
+ * @return void
  */
 function arrangeItems(sel) {
 
@@ -420,17 +453,27 @@ function arrangeItems(sel) {
     }
 };
 
+/**
+ * Saves the file in AI format.
+ * @param <string> The file destination path
+ * @return void
+ */
 function saveFileAsAi(dest) {
     if (app.documents.length > 0) {
         var options = new IllustratorSaveOptions();
         var theDoc = new File(dest);
-        options.compatibility = Compatibility.ILLUSTRATOR10;
+        options.compatibility = CONFIG.AIFORMAT;
         options.flattenOutput = OutputFlattening.PRESERVEAPPEARANCE;
         options.pdfCompatible = true;
         app.activeDocument.saveAs(theDoc, options);
     }
 }
 
+/**
+ * Aligns selection to nearest whole pixel
+ * @param <selection> sel The selection object
+ * @return void
+ */
 function alignToNearestPixel(sel) {
     
     try {
@@ -452,6 +495,11 @@ function alignToNearestPixel(sel) {
     }
 }
 
+/**
+ * Logging for this script.
+ * @param <string> The logging text
+ * @return void
+ */
 function logger(txt) {  
 
     if (CONFIG.LOGGING == 0) return;
@@ -465,8 +513,8 @@ function logger(txt) {
 
 /**
  * Aligns the item to the nearest pixel for crisp rendering.
- * param object item    The item to align
- * return void
+ * @param <object> item    The item to align
+ * @return void
  */
 function alignToNearestPixel(item) {
     if (item.height) {
@@ -481,8 +529,8 @@ function alignToNearestPixel(item) {
 
 /**
  * Adjusts a value to the nearest whole number
- * param float n   The value to adjust
- * return int
+ * @param <float> n   The value to adjust
+ * @return <int>
  */
 function moveToPixel(n) {
     return Math.round(n)
