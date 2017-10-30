0eaff2ac89d1bfb423cbd72dd3e5b18bad3f2082
Scott Lewis <scott@iconify.it>
2017-10-30 14:58:22 -0400

    Add file name to progress bar

--- merge-svg-docs/Merge SVG Docs.jsx
+++ merge-svg-docs/Merge SVG Docs.jsx
@@ -1,3 +1,42 @@
+/**		
+ * The MIT License (MIT)		
+ *		
+ * Permission is hereby granted, free of charge, to any person obtaining a copy		
+ * of this software and associated documentation files (the "Software"), to deal		
+ * in the Software without restriction, including without limitation the rights		
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell		
+ * copies of the Software, and to permit persons to whom the Software is		
+ * furnished to do so, subject to the following conditions:		
+ *		
+ * The above copyright notice and this permission notice shall be included in		
+ * all copies or substantial portions of the Software.		
+ *		
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR		
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,		
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE		
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER		
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,		
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN		
+ * THE SOFTWARE.		
+ *		
+ */		
+/**		
+ *  Installation:		
+ *		
+ *      1. Copy this file to Illustrator > Presets > Scripting		
+ *      2. Restart Adobe Illustrator		
+ *      3. Go to File > Scripts > Merge SVG Docs		
+ *      4. Follow the prompts		
+ *		
+ *  Usage:		
+ *		
+ *      This script will import a folder of SVG files and merge them into a single document with		
+ *      the contents of each SVG file placed on a separate artboard. The artboard name will be set		
+ *      to the original SVG file's name without the file extension. Up to Adobe Illustrator 2015.3,		
+ *      it is only possible to import a maximum of 100 files since Illustrator only supports a		
+ *      maximum of 100 artboards.		
+ */
+
 #target Illustrator
 
 var originalInteractionLevel = userInteractionLevel;
